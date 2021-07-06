c211aac8b2dcae4bdb2b9b9d15fb33090daad340
vincentbaylon <vincentbaylon@gmail.com>
2021-07-06 10:45:15 -0700

    commit tree 9758900ae3e1ffc61dbd1c328f6a51f86a64b608

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(function(response) {
+    return response.json()
+  })
+  .then(function(json) {
+    renderBooks(json)
+    return json
+  })
 }
 
 function renderBooks(books) {
